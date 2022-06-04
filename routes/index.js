ca20fabfde9d576fd86dc8332e2956d87084a744
Honey Jayan <honeyjayan2000@gmail.com>
2022-06-03 22:17:37 -0400

    added individual pages for each route

--- routes/index.js
+++ routes/index.js
@@ -13,22 +13,22 @@ router.get('/home', function(req, res, next) {
 
 /* GET About page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me'});
+  res.render('about', { title: 'About Me'});
 });
 
 /* GET Products page. */
 router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products'});
+  res.render('projects', { title: 'Products'});
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services'});
+  res.render('services', { title: 'Services'});
 });
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact'});
+  res.render('contact', { title: 'Contact'});
 });
 
 module.exports = router;
